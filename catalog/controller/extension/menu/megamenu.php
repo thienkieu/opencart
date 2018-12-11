10add79ceb6a4a696dd24abe9b3e8d00e0884d7a
thien.kieu <thien1988@gmail.com>
2018-12-11 23:00:01 +0700

    add megamenu

--- catalog/controller/extension/menu/megamenu.php
+++ catalog/controller/extension/menu/megamenu.php
@@ -2,9 +2,9 @@
 class ControllerExtensionMenuMegamenu extends Controller {
 	public function index() {
 		$this->load->language('extension/menu/megamenu');
-		$data = $this->document->getStyles();
-		
+		$data['all_category_width'] = 260;
+		$data['show_default'] = $this->request->get['route'] == 'common/home' ? 'show-default' : '';
 		//echo "this is megamenu";die;
-		return $this->load->view('extension/menu/megamenu');
+		return $this->load->view('extension/menu/megamenu', $data);
 	}
 }
