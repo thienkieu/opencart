f470b3f62deee73abb6477387bd5356e482e8f75
thien.kieu <thien1988@gmail.com>
2018-12-12 08:09:12 +0700

    add seo url

--- catalog/controller/extension/menu/megamenu.php
+++ catalog/controller/extension/menu/megamenu.php
@@ -3,7 +3,8 @@ class ControllerExtensionMenuMegamenu extends Controller {
 	public function index() {
 		$this->load->language('extension/menu/megamenu');
 		$data['all_category_width'] = 260;
-		$data['show_default'] = $this->request->get['route'] == 'common/home' ? 'show-default' : '';
+		$route = isset($this->request->get['route'])? $this->request->get['route']: 'common/home';
+		$data['show_default'] = $route == 'common/home' ? 'show-default' : '';
 		//echo "this is megamenu";die;
 		return $this->load->view('extension/menu/megamenu', $data);
 	}
